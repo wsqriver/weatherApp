0f4e0b035c85d70eb2d2311f0f4116f306c24525
wsqriver <531153213@qq.com>
2023-11-20 10:26:05 +0800

    connection AWS get data from Dynamodb

--- src/main/java/com/city/dao/impl/CityDaoImpl.java
+++ src/main/java/com/city/dao/impl/CityDaoImpl.java
@@ -1,11 +1,38 @@
 package com.city.dao.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.model.AmazonDynamoDBException;
+import com.city.bean.City;
 import com.city.dao.CityDao;
+import com.tools.ExceptionEnumMsg;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class CityDaoImpl implements CityDao {
+
+    @Autowired
+    DynamoDBMapper dynamoDBMapper;
+
+    private  final Logger logger = LoggerFactory.getLogger(CityDaoImpl.class);
     @Override
     public JSONObject getCitybyId(String cityId) {
-        return null;
+                //if get result from DB.
+        boolean flag=true;
+        JSONObject params= null;
+        try {
+            params = JSONObject.parseObject(JSON.toJSONString(dynamoDBMapper.load(City.class, cityId)));
+        }catch (AmazonDynamoDBException e){
+            params=null;
+            logger.error(ExceptionEnumMsg.NOTFOUNDCITY.getErrorCode());
+        }catch (Exception e){
+            params=null;
+            logger.error(ExceptionEnumMsg.NOTFOUNDCITY.getErrorCode());
+        }
+        return params;
     }
 }
---
+++ src/main/java/com/tools/AwsUtilsConfig.java
@@ -0,0 +1,36 @@
+package com.tools;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsUtilsConfig {
+    //todo get accesskey&&secretkey in AWS secret manager
+    @Value("${accessKey}")
+    private  String accessKey;
+    @Value("${secretKey}")
+    private  String secretKey;
+    //get AWS connection
+    public  AWSCredentialsProvider getAwsProvider(){
+        return new AWSCredentialsProvider() {
+            public void refresh() {}
+            public AWSCredentials getCredentials() {return new BasicAWSCredentials(accessKey, secretKey);}
+        };
+    }
+    //dynamodb bean
+    @Bean
+    public  DynamoDBMapper getDynamoDBMapper(){
+        AmazonDynamoDB amazonDynamoDBClient;
+        amazonDynamoDBClient = AmazonDynamoDBClientBuilder.standard().withCredentials(getAwsProvider()).withRegion(Regions.US_EAST_1).build();
+        return new DynamoDBMapper(amazonDynamoDBClient);
+    }
+
+
+}
